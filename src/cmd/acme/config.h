513a84950565b4a5cfdf3e757cf692435738843e
mk-f <mk-f@my.domain>
2021-01-28 11:41:10 +0100

    update colors

--- src/cmd/acme/config.h
+++ src/cmd/acme/config.h
@@ -1,21 +1,17 @@
-#define C_TAGBG                 0x1c1c1cFF
+#define C_TAGBG                 0x1c1c1cff
 #define C_TAGFG                 0xc5c8c6ff
-
-#define C_TAGBG                 0x4e4e4eff
-#define C_TAGFG                 0xffffffff
 #define C_TAGHLBG               0x4c7899ff
-#define C_TAGHLFG               0x000000ff
+#define C_TAGHLFG               0xeff1f5ff
 
-#define C_TXTBG                 0x121212FF
 #define C_TXTBG                 0x1c1c1cff
 #define C_TXTFG                 0xc5c8c6ff
 #define C_TXTHLBG               0x4c7899ff
-#define C_TXTHLFG               0x000000ff
+#define C_TXTHLFG               0xeff1f5ff
 
 #define C_WINBUTTON             0x3a3a3aff
-#define C_COLBUTTON             0x3a3a3aff
+#define C_COLBUTTON             0x4c7899ff /* also underline of row tag */
 #define C_TMPBUTTON             0x4c7899ff /* changed content */
 #define C_SCROLLBG              0x4e4e4eff
 
-#define C_BUTTON2HL             0xaa0000FF
-#define C_BUTTON3HL             0x006600FF
+#define C_BUTTON2HL             0xaa0000ff
+#define C_BUTTON3HL             0x006600ff
