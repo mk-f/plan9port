3cf93b92a34b1e3c8d9368105d07e1b0f71707d3
mk-f@my.domain <mk-f>
2022-06-18 12:07:32 +0200

    update acme.sh

--- acme.sh
+++ acme.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -eu
+set -eux
 
 ACME=${PLAN9}/bin/acme
 ACME_AUX=$HOME/.acme
@@ -10,50 +10,46 @@ CGO_LDFLAGS="-L/usr/local/lib"
 MANPATH=$PLAN9/man export MANPATH
 PATH=${PLAN9}/bin:$PATH export PATH
 
-BUTMOD=0x4c7899ff
-SCRBAR=0x333333ff
-
-TXTFG=0xd0d0d0ff
-TXTBG=0x1c1c1cff
-TXTHLFG=$TXTFG
-TXTHLBG=0x285577ff
-
-TAGFG=0x9e9e9eff
-# TAGFG=$BUTMOD
-TAGBG=0x121212ff
-TAGHLFG=$TAGBG
-TAGHLBG=$TAGFG
-
-B2HL=0x400000ff
-B3HL=0x004000ff
-
-TEMPLATE=$(printf '%s' \
-	"textback:$TXTBG,"\
-	"texthigh:$TXTHLBG,"\
-	"textbord:$SCRBAR,"\
-	"texttext:$TXTFG,"\
-	"texthtext:$TXTHLFG,"\
-	"tagback:$TAGBG,"\
-	"taghigh:$TAGHLBG,"\
-	"tagbord:$SCRBAR,"\
-	"tagtext:$TAGFG,"\
-	"taghtext:$TAGHLFG,"\
-	"butmod:$BUTMOD,"\
-	"butcol:$SCRBAR,"\
-	"but2col:$B2HL,"\
-	"but3col:$B3HL")
-
 pgrep plumber >/dev/null || plumber
 
-#	-f /mnt/font/GoMono/10a/font 
+if [ "${0##*/}" = "acmebright" ]; then
+. "$HOME/src/acme-scripts/solarized-bright.theme"
+fi
 
 if [ "${0##*/}" = "acmedark" ]; then
-	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a -M \
-		-f ${PLAN9}/font/fixed/unicode.9x15.font \
-		-t "$TEMPLATE" \
-		"$@" $ACME_AUX/guide
+. "$HOME/src/acme-scripts/solarized-dark.theme"
+fi
+
+#	-f /mnt/font/GoMono/10a/font
+
+if [ "${0##*/}" = "acmedark" ] || [ "${0##*/}" = "acmebright" ]; then
+	TEMPLATE=$(printf '%s' \
+		"textback:$TXTBG,"\
+		"texthigh:$TXTHLBG,"\
+		"textbord:$TXTBORD,"\
+		"texttext:$TXTFG,"\
+		"texthtext:$TXTHLFG,"\
+		"tagback:$TAGBG,"\
+		"taghigh:$TAGHLBG,"\
+		"tagbord:$TAGBORD,"\
+		"tagtext:$TAGFG,"\
+		"taghtext:$TAGHLFG,"\
+		"butmod:$BUTMOD,"\
+		"butcol:$BUTCOL,"\
+		"but2col:$B2HL,"\
+		"but3col:$B3HL")
+
+#	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a -M \
+	# rc starts as loginshell, reading $home/lib/profile
+	# when argument 0 (its name) starts with '-'. -rc is a
+	# link to rc
+	SHELL=${PLAN9}/bin/rc $ACME -c 2 -a \
+			-f ${PLAN9}/font/fixed/unicode.9x15.font \
+			-t "$TEMPLATE" \
+			"$@" $ACME_AUX/guide
 else
-	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a -M \
+#	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a -M \
+	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a \
 		-f ${PLAN9}/font/fixed/unicode.9x15.font \
 		"$@" $ACME_AUX/guide
 fi
